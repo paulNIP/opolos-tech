a387421bf4879f6ab065288a8af56f1200df9dee
Paul Alex <alex@attorney-shield.com>
2026-02-21 19:09:24 +0300

    Update on github secrets to test deployment

--- components/products.tsx
+++ components/products.tsx
@@ -44,6 +44,7 @@ export function Products() {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            
           <ProductCard
             icon="C"
             title={t('productCard.citrix')}
